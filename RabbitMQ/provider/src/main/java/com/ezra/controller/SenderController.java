a1e53a304ddedf51cb2570b42835650135eeda47
EzraWen <wenshuqian00>
2020-11-16 16:06:58 +0800

    项目结构调整

--- RabbitMQ/provider/src/main/java/com/ezra/controller/SenderController.java
+++ RabbitMQ/provider/src/main/java/com/ezra/controller/SenderController.java
@@ -1,10 +1,9 @@
 package com.ezra.controller;
 
+import cn.hutool.core.util.StrUtil;
 import com.ezra.constant.RabbitMQConstant;
 import com.ezra.constant.SystemConstant;
 import com.ezra.response.Result;
-import com.ezra.util.ToolUtil;
-import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,22 +20,22 @@ public class SenderController {
 
     @GetMapping("/sendMsg")
     public Result sendMsg(@RequestParam(value = "msg", required = false) String msg) {
-        if (ToolUtil.isEmpty(msg)) {
+        if (StrUtil.isEmpty(msg)) {
             System.out.println("msg为空，不发送到队列中");
             return Result.SUCCESS;
         }
-        rabbitTemplate.convertAndSend(RabbitMQConstant.QUEUE1,msg);
+        rabbitTemplate.convertAndSend(RabbitMQConstant.QUEUE1, msg);
         return Result.SUCCESS;
     }
 
 
     @GetMapping("/sendMsg2")
     public Result sendMsg2(@RequestParam(value = "msg", required = false) String msg) {
-        if (ToolUtil.isEmpty(msg)) {
+        if (StrUtil.isEmpty(msg)) {
             System.out.println("msg为空，不发送到队列中");
             return Result.SUCCESS;
         }
-        rabbitTemplate.convertAndSend(RabbitMQConstant.QUEUE2,msg);
+        rabbitTemplate.convertAndSend(RabbitMQConstant.QUEUE2, msg);
         return Result.SUCCESS;
     }
 }
