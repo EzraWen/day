28a959932f89a65c0a8a472e626dfb84c0c7fc6c
EzraWen <3535303287@qq.com>
2020-11-23 23:22:25 +0800

    修改线程工厂的创建方式

--- Thread/src/main/java/com/ezra/pool/ThreadPoolConfig.java
+++ Thread/src/main/java/com/ezra/pool/ThreadPoolConfig.java
@@ -19,6 +19,14 @@ public class ThreadPoolConfig {
         return newFixedThreadPool(10);
     }
 
+    /**
+     * 限定等待队列中的任务数量,避免任务过多而OOM
+     * 修改keepAlive的时间,使非核心的线程超过时间没有任务执行后,被回收
+     * 使用guava提供的线程工厂创造,对线程名称格式化,或者设置为守护线程
+     * 使用默认的任务拒绝策略
+     * @param nThreads
+     * @return
+     */
     private static ExecutorService newFixedThreadPool(int nThreads) {
         return new ThreadPoolExecutor(nThreads, nThreads,
                 1000L, TimeUnit.SECONDS,
