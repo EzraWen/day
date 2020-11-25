6edd84a5787924e097d8a917b8294b8d6a4d3100
EzraWen <wenshuqian00>
2020-11-25 17:48:25 +0800

    es模块

---
+++ Elasticsearch/src/main/java/com/ezra/ElasticsearchApplication.java
@@ -0,0 +1,24 @@
+package com.ezra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = "com.ezra")
+public class ElasticsearchApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElasticsearchApplication.class,args);
+    }
+}
+
+//
+//PUT /my_index/_doc/3?routing=1
+//{
+//    "text": "This is an answer",
+//    "my_join_field": {
+//    "name": "answer",
+//    "parent": "1"
+//}
